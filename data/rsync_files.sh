63f798728d0b42ac63bf0c6c99add5a1fb7f86e1
olapuente <o.lapuente.santana@tue.nl>
2020-04-23 10:10:43 +0200

    syncronization with cluster

--- data/rsync_files.sh
+++ data/rsync_files.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-for cancer_type in $(cat /home/olapuent/Desktop/PhD_TUE/Github_model/desktop/data/PanCancer_names.txt)
-do
-	cd /home/olapuent/Desktop/PhD_TUE/Github_model/desktop/data/PanCancer/$cancer_type/new/
-	rsync -av --progress ./ feduati@biosim3.bmt.tue.nl:/home/feduati/Oscar/PhD_Oscar/new/data/PanCancer/$cancer_type/
-done
+cd /home/olapuent/Desktop/PhD_TUE/Github_model/repository/mechanistic_biomarkers_immuno-oncology/data
+rsync -av --progress ./PanCancer feduati@biosim3.bmt.tue.nl:/home/feduati/Oscar/PhD_Oscar/new/data/
+
 
