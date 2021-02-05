7a1e5383a89a4976ffb99e7d158279d879b139c5
olapuente <o.lapuente.santana@tue.nl>
2021-02-05 23:03:42 +0100

    Cleaned up

--- data/rsync_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rsync -av --progress feduati@biosim3.bmt.tue.nl:/home/feduati/Oscar/PhD_Oscar/new/output/Federica_presentation_colab/SKCM/new_remove_all_genes/ /Users/Oscar/Desktop/PhD_TU:e/Research/mechanistic_biomarkers_immuno-oncology/output/Federica_presentation_colab/
-
-
-
-
